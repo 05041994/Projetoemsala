7f2b4c4fbe4db09e5fd14933095b0419957538e8
Gabriel Cortez <96384061+05041994@users.noreply.github.com>
2023-01-16 16:02:30 -0300

    Add files via upload

---
+++ EscolaApp/EscolaApp/NProfessor.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Xml.Serialization;
+using System.Threading.Tasks;
+
+namespace EscolaApp
+{
+    static class NProfessor
+    {
+        private static List<Professor> Profs = new List<Professor>();
+
+        public static void Inserir(Professor p)
+        {
+            Abrir();
+            
+            int id = 0;
+            foreach (Professor obj in Profs)
+                if (obj.Id > id) id = obj.Id;
+            p.Id = id + 1;
+            Profs.Add(p);
+            Salvar();
+        }
+        public static List<Professor> Listar()
+        {
+            Abrir();
+            return Profs;
+        }
+        public static void Atualizar(Professor p)
+        {
+            Abrir();
+            
+            foreach (Professor obj in Profs)
+                if (obj.Id == p.Id)
+                {
+                    obj.Nome = p.Nome;
+                    obj.Matricula = p.Matricula;
+                    obj.Area = p.Area;
+                    obj.Id = p.Id;
+                }
+            Salvar();
+        }
+        public static void Excluir(Professor p)
+        {
+            Abrir();
+            
+            Professor x = null;
+            foreach (Professor obj in Profs)
+                if (obj.Id == p.Id) x = obj;
+            if (x != null) Profs.Remove(x);
+            Salvar();
+        }
+        public static void Abrir()
+        {
+            StreamReader f = null;
+            try
+            {
+               
+                XmlSerializer xml = new XmlSerializer(typeof(List<Professor>));
+                
+                f = new StreamReader("./Profs.xml");
+                
+                Profs = (List<Professor>)xml.Deserialize(f);
+            }
+            catch
+            {
+                Profs = new List<Professor>();
+            }
+           
+            if (f != null) f.Close();
+        }
+        public static void Salvar()
+        {
+         
+            XmlSerializer xml = new XmlSerializer(typeof(List<Professor>));
+            
+            StreamWriter f = new StreamWriter("./Profs.xml", false);
+            
+            xml.Serialize(f, Profs);
+            
+            f.Close();
+        }
+        //public static void Matricular(Aluno a, Turma t)
+        //{
+        //    a.IdTurma = t.Id;
+        //    Atualizar(a);
+        //}
+        //public static List<Aluno> Listar(Turma t)
+        //{
+        //    Abrir();
+            
+        //    List<Aluno> diario = new List<Aluno>();
+        //    foreach (Aluno obj in alunos)
+        //        if (obj.IdTurma == t.Id) diario.Add(obj);
+        //    return diario;
+        //}
+
+    }
+}
